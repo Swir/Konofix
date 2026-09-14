43f40db5218c4144eeea4c3efd6503177b57ee15
Wojciech <swirjestem@hotmail.com>
2026-09-15 00:47:34 +0200

    ops: translate node launcher to English

--- run-node.bat
+++ run-node.bat
@@ -7,7 +7,7 @@ echo     Konofix Node 0.4.2 - Internet Node
 echo ==========================================
 echo.
 set "PORT=45555"
-set /p "PUBLIC_HOST=Podaj publiczny IP lub DNS noda (ENTER = bez publicznego adresu): "
+set /p "PUBLIC_HOST=Enter public IP or DNS name (ENTER = no public address): "
 echo.
 
 if "%PUBLIC_HOST%"=="" (

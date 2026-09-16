b95b767ddc5a86d5b506dd275b2427113bf379d9
Wojciech <swirjestem@hotmail.com>
2026-09-16 18:07:03 +0200

    build: lock standalone Konofix Node dependencies

--- build-node.bat
+++ build-node.bat
@@ -1,4 +1,13 @@
 @echo off
+setlocal
 cd /d "%~dp0src-tauri"
-cargo build --release --bin konofix-node
+if not exist "Cargo.lock" (
+  echo ERROR: committed src-tauri\Cargo.lock is missing.
+  pause
+  exit /b 1
+)
+cargo build --locked --release --bin konofix-node
+set "ERR=%ERRORLEVEL%"
+if not "%ERR%"=="0" echo ERROR: Konofix Node build failed with exit code %ERR%.
 pause
+exit /b %ERR%
